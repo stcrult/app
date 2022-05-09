5d1094e62644766167e5fbb887ea640257df44be
stcrult <82962821+stcrult@users.noreply.github.com>
2022-05-09 22:32:58 +0300

    Create Program.py

---
+++ Python/MaximumOf2Numbers/Program.py
@@ -0,0 +1,9 @@
+def checkNumbers(firstNumber, secondNumber):
+    if (firstNumber > secondNumber):
+        print("First number is greater than Second number")
+    elif (secondNumber > firstNumber):
+        print("Second number is grater than First number")
+    else:
+        print("Numbers are equal")
+        
+checkNumbers(3,4)
