3d6fcda68ef0f11b9741695bf7f7b3c6f0b3661d
stcrult <82962821+stcrult@users.noreply.github.com>
2022-05-09 23:55:18 +0300

    Create Program.py

---
+++ Python/Lists/Program.py
@@ -0,0 +1,5 @@
+students = ["Napier", "Ada", "Alan", "Charles", "Grace", "Guido"]
+
+print("There are " + str(len(students)) + " students in the class")
+print("One of them is " + students[0])
+print("And another one is " + students[4])
